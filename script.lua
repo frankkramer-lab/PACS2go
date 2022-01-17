5fdd0445c751ccf18d2297257fceef3978f56245
Tamara Krafft <tamara.krafft@uni-a.de>
2022-01-17 12:29:16 +0100

    lua scripting #12

--- script.lua
+++ script.lua
@@ -1,6 +1,7 @@
 function IncomingHttpRequestFilter(method, uri, ip, username, httpHeaders)
     -- Only allow GET requests for non-admin users (or 'uploader'-> used for pacs2go tools' direct upload)
-    if method == 'GET' or string.find(uri,"/tools/find") ~= nil then -- extra: allow 'do lookup' in ORTHANC
+    -- extra: allow 'do lookup' in ORTHANC (/tools/find), as this kind of lookup is a post request, that does not alter data
+    if method == 'GET' or string.find(uri,"/tools/find") ~= nil then 
         return true
     elseif username == 'admin' or username == 'uploader' then
         return true
@@ -10,10 +11,9 @@ function IncomingHttpRequestFilter(method, uri, ip, username, httpHeaders)
     end
 end
 
--- and string.find(uri, "/") ~= nil
 
 function ReceivedInstanceFilter(dicom, origin, info)
-    -- Only allow incoming MR images
+    -- Only allow incoming images if they are pseudonymized
     if dicom.PatientIdentityRemoved == 'YES' then
         print("Identity is removed")
         return true
