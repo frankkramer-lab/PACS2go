cff7ceb2160ec3d0f07568a9dc8897f5733d4d75
Tamara Krafft <tamara.krafft@uni-a.de>
2022-01-20 20:02:25 +0100

    first startpage draft #15

--- web-service/app.py
+++ web-service/app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def startpage():
+    return render_template('startpage.html')
+
+def pseudonymization():
+    return render_template('')
