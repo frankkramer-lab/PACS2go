d34db55241596ad3b2d7c02e128bac7a2276e0da
Tamara Krafft <tamara.krafft@uni-a.de>
2022-01-24 12:43:00 +0100

    convert2dicom clean up #13

--- web-service/tools/convert2dicom.py
+++ web-service/tools/convert2dicom.py
@@ -1,3 +1,4 @@
+from helpers import upload_to_orthanc
 import pydicom
 from pydicom.dataset import Dataset
 from pydicom.uid import generate_uid
@@ -11,8 +12,6 @@ from zipfile import ZipFile
 import gzip
 import sys
 sys.path.append('./tools')
-from helpers import upload_to_orthanc
-
 
 
 # Conversion function for both directories and single files, destination argument is optional
@@ -40,7 +39,6 @@ def convert(path, destination='', upload=False, from_web_request=False):
             if not os.path.exists(destination):
                 os.mkdir(destination)
 
-        
         zipped_file = os.path.join(destination, "data.zip")
 
         # converts a whole directory of image files to dicom
@@ -53,13 +51,15 @@ def convert(path, destination='', upload=False, from_web_request=False):
                     # checking if it is a file
                     # extra check for endings since folders often contain additional study data in csv or different format
                     if os.path.isfile(f) and (f.endswith(".jpg") or f.endswith(".bmp") or f.endswith(".png")):
-                        dicom = pilfile2dicom(f, destination, upload, from_web_request, uids, i)
+                        dicom = pilfile2dicom(
+                            f, destination, upload, from_web_request, uids, i)
                         if not upload:
                             zip.write(dicom)
                         i += 1
-                    elif os.path.isfile(f) and (f.endswith(".nii") or path.endswith (".nii.gz")):
+                    elif os.path.isfile(f) and (f.endswith(".nii") or path.endswith(".nii.gz")):
                         # array of dicom file names
-                        dicom_files = nifti2dicom(f, destination, upload, from_web_request, uids, i)
+                        dicom_files = nifti2dicom(
+                            f, destination, upload, from_web_request, uids, i)
                         for file in dicom_files:
                             if not upload:
                                 zip.write(file)
@@ -67,14 +67,16 @@ def convert(path, destination='', upload=False, from_web_request=False):
 
         # converts a single non dicom file to dicom
         elif os.path.isfile(path):
-            print(path, flush=True)
             if path.endswith(".jpg") or path.endswith(".bmp") or path.endswith(".png"):
                 with ZipFile(zipped_file, 'w') as zip:
-                    dicom = pilfile2dicom(path, destination, upload, from_web_request, uids)  # i is set to default
+                    # i is set to default
+                    dicom = pilfile2dicom(
+                        path, destination, upload, from_web_request, uids)
                     if not upload:
                         zip.write(dicom)
-            elif path.endswith(".nii") or path.endswith (".nii.gz"):
-                dicom_files = nifti2dicom(path, destination, upload, from_web_request, uids)  # i is set to default
+            elif path.endswith(".nii") or path.endswith(".nii.gz"):
+                dicom_files = nifti2dicom(
+                    path, destination, upload, from_web_request, uids)  # i is set to default
                 with ZipFile(zipped_file, 'w') as zip:
                     for file in dicom_files:
                         if not upload:
@@ -96,15 +98,18 @@ def pilfile2dicom(filename, destination, upload, from_web_request, uids, series_
         img = img.convert('RGB')
 
     # translate grayscale or rgb image to numpy array
+
     if img.mode == 'L':
         np_frame = numpy.asarray(img)
     elif img.mode == 'RGBA' or img.mode == 'RGB':
+        # pixel depth of RGB = 24 bit (3*8 bit) -> uint8
         np_frame = numpy.array(img.getdata(), dtype=numpy.uint8)
     else:
         raise Exception("Unknown image mode")
-    shape = [height, width, np_frame.shape[1]]
+    # get height, width and number of channels (rgb or grayscale) and pixel depth (always 8bit per channel)
+    image_properties = [height, width, np_frame.shape[1], 8]
 
-    ds = image2dicom(np_frame, shape, uids, series_index)
+    ds = image2dicom(np_frame, image_properties, uids, series_index)
     if upload:
         upload_to_orthanc(ds, filename, from_web_request)
     else:
@@ -117,16 +122,18 @@ def nifti2dicom(filename, destination, upload, from_web_request, uids, series_in
     nifti_file = nib.load(filename)
     nifti_array = nifti_file.get_fdata()
     slices_count = nifti_array.shape[2]
-    # shape[3] should always be 1 since nifti files are never RGB
-    shape = [nifti_array.shape[0], nifti_array.shape[1], 1]
-    
-    # list of dicom file names (each slice) for web-service zipping
+    # image_properties holds [height, width, number of channels, pixel depth] and is needed for image2dicom function
+    # number of channels is set to 1 since nifti files are usually grayscale (CT images), pixel depth of nifti files is usually 16 bit
+    image_properties = [nifti_array.shape[0], nifti_array.shape[1], 1, 16]
+
+    # list of dicom file names (each slice) for zipping
     dicom_files = []
 
-    # converts and saves each slice of the nifti file (slice=instance, file=series/study)
+    # converts and saves/uploads each slice of the nifti file (slice=instance, file=series)
     for slice in range(slices_count):
+        # pixel depth of nifti files is usually 16bit
         array = nifti_array[:, :, slice].astype('uint16')
-        ds = image2dicom(array, shape, uids, series_index, slice, nifti=True)
+        ds = image2dicom(array, image_properties, uids, series_index, slice)
         if upload:
             upload_to_orthanc(ds, filename, from_web_request)
         else:
@@ -134,10 +141,9 @@ def nifti2dicom(filename, destination, upload, from_web_request, uids, series_in
     return dicom_files
 
 
-
 # converts and saves a non-dicom image file to dicom
 # based on: https://github.com/jwitos/JPG-to-DICOM/blob/master/jpeg-to-dicom.py
-def image2dicom(array, arr_shape, uids, series_index, instance_index=0, nifti=False):
+def image2dicom(array, image_properties, uids, series_index, instance_index=0):
     # Create DICOM from scratch
     ds = Dataset()
     ds.file_meta = Dataset()
@@ -146,9 +152,9 @@ def image2dicom(array, arr_shape, uids, series_index, instance_index=0, nifti=Fa
     ds.file_meta.MediaStorageSOPInstanceUID = "1.2.276.0.7230010.3.1.4.0.42382.1638878078.817218"
     ds.file_meta.ImplementationClassUID = "1.2.3.4"
 
-    ds.Rows = arr_shape[0]
-    ds.Columns = arr_shape[1]
-    if arr_shape[2] == 3:
+    ds.Rows = image_properties[0]
+    ds.Columns = image_properties[1]
+    if image_properties[2] == 3:
         ds.SamplesPerPixel = 3
         ds.PhotometricInterpretation = "RGB"
     else:
@@ -156,16 +162,11 @@ def image2dicom(array, arr_shape, uids, series_index, instance_index=0, nifti=Fa
         # for inverted grayscale try "MONOCHROME"
         ds.PhotometricInterpretation = "MONOCHROME2"
 
-    if nifti:
-        # nifti needs uint16 for display
-        ds.BitsStored = 16
-        ds.BitsAllocated = 16
-        ds.HighBit = 15
-    else:
-        # pathology images need uint8
-        ds.BitsStored = 8
-        ds.BitsAllocated = 8
-        ds.HighBit = 7
+    # pixel depth information
+    ds.BitsStored = image_properties[3]
+    ds.BitsAllocated = image_properties[3]
+    ds.HighBit = image_properties[3] - 1
+
     ds.PixelRepresentation = 0
     ds.PlanarConfiguration = 0
     ds.NumberOfFrames = 1
@@ -214,6 +215,6 @@ def save_dicom_file(ds, destination):
 # how to use convert2dicom:
 
 # single nifti .nii file, no upload
-# convert(path=r'/home/main/Desktop/images/nifti/1010_brain_mr_02.nii')
+convert(path=r'/home/main/Desktop/images/nifti/1010_brain_mr_02.nii')
 # directory of jpeg files, with upload to ORTHANC
 # convert(path=r'/home/main/Desktop/images/Osteosarcoma-UT/Training-Set-1/set10', upload=True)
