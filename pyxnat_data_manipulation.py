b780fdd6795467fc57765c65285c9b93ab1d631d
Tamara Krafft <tamara.krafft@uni-a.de>
2022-07-13 15:22:06 +0200

    removing data with pyxnat, tested show_jpeg #21

--- pyxnat_data_manipulation.py
+++ pyxnat_data_manipulation.py
@@ -13,6 +13,12 @@ def create_project(name):
                 project.create()
 
 
+def delete_project(name):
+        project = interface.select.project(name)
+        if project.exists() == True:
+                project.delete()
+
+
 def get_all_projects():
         return interface.select.projects().get()
 
@@ -21,21 +27,36 @@ def get_project_subjects(name):
         return interface.select.project(name).subjects().get()
 
 
-def insert_file_into_project(name, filename):
-        project = interface.select.project(name)
-        if filename.endswith('.jpg') or filename.endswith('.jpeg'):
-                project.resource('JPEG').file(str(uuid.uuid4()) + '.jpeg').insert(filename,content='image',format='JPEG',tags='image test')
-        if filename.endswith('.json'):
-                project.resource('JSON').file(str(uuid.uuid4()) + '.json').insert(filename,content='metadata',format='JSON',tags='metadata')
-        
+def insert_file_into_project(project_name, file_path):
+        project = interface.select.project(project_name)
+        file_id = str(uuid.uuid4())
+        if file_path.endswith('.jpg') or file_path.endswith('.jpeg'):
+                project.resource('JPEG').file(file_id + '.jpeg').insert(file_path,content='image',format='JPEG',tags='image test')
+        if file_path.endswith('.json'):
+                project.resource('JSON').file(file_id + '.json').insert(file_path,content='metadata',format='JSON',tags='metadata')
+        return file_id
 
-""" def show_jpeg():
+
+def remove_file_from_project(project_name, file_name, resource_name):
+        file = interface.select.project(project_name).resource(resource_name).file(file_name)
+        print(file)
+        file.delete()
+                
+
+def show_jpeg():
         project = interface.select.project('test3')
         file = project.resource('JPEG').files().first()
         print(file.get())
         image = Image.open(file.get())
- """
+        image.show()
+
 
 
 # insert_file_into_project('test3', '/home/main/Desktop/pacs2go/pacs2go/test_data/pathology_images/Case-3-A15-25441-4885.jpg') # absoluten Pfad angeben!!
 # insert_file_into_project('test3', '/home/main/Desktop/pacs2go/pacs2go/test_data/example_1.json')
+
+#file = insert_file_into_project('test', '/home/main/Desktop/pacs2go/pacs2go/test_data/example_1.json')
+#print(file)
+# remove_file_from_project('test3', 'fe2e183c-b1ac-4ba3-8b3e-784d4d223f25.json', 'JSON')
+
+interface.disconnect()
