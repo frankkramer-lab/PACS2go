908410e901ea58d2ff68260d1036d89ee39c50be
Tamara Krafft <tamara.krafft@uni-a.de>
2022-08-04 13:47:51 +0200

    mypy typing and fixed bugs that came visible thanks to typing #22

--- pacs_data_interface.py
+++ pacs_data_interface.py
@@ -1,102 +1,106 @@
 from abc import ABC, abstractmethod, abstractproperty
+from typing import Optional, List, Any, Sequence
 
 
 class Connection(ABC):
-    def __init__(self, username='', password=''):
+    def __init__(self, username: Optional[str]='', password: Optional[str]='') -> None:
         self._projects = self.get_all_projects()
+        self.interface: Any
 
-    @abstractmethod
-    def __enter__(self):
+    @abstractproperty
+    def user(self) -> str:
         pass
 
     @abstractmethod
-    def __exit__(self, type, value, traceback):
+    def __enter__(self) -> 'Connection':
         pass
 
     @abstractmethod
-    def get_project(self, name):
+    def __exit__(self, type, value, traceback) -> None:
         pass
 
     @abstractmethod
-    def get_all_projects(self):
+    def get_project(self, name: str) -> Optional['Project']:
+        pass
+
+    @abstractmethod
+    def get_all_projects(self) -> Sequence['Project']:
         pass
 
 
 class Project(ABC):
-    def __init__(self, connection, name):
+    def __init__(self, connection: Connection, name: str) -> None:
         self.connection = connection
         self.name = name
-        self._directories = self.get_all_directories()
 
     @abstractproperty
-    def description(self):
-        pass
-
-    @abstractproperty
-    def owners(self):
+    def description(self) -> str:
         pass
 
     @abstractproperty
-    def your_user_role(self):
+    def owners(self) -> List[str]:
+        pass
+
+    @abstractproperty
+    def your_user_role(self) -> str:
         pass
 
     @abstractmethod
-    def delete_project(self):
+    def delete_project(self) -> None:
         pass
 
     @abstractmethod
-    def get_directory(self, name):
+    def get_directory(self, name) -> 'Directory':
         pass
 
     @abstractmethod
-    def get_all_directories(self):
+    def get_all_directories(self) -> Sequence['Directory']:
         pass
 
     @abstractmethod
-    def insert_zip_into_project(self, file_path):
+    def insert_zip_into_project(self, file_path: str) -> 'Directory':
         pass
 
     @abstractmethod
-    def insert_file_into_project(self, file_path):
+    def insert_file_into_project(self, file_path: str) -> 'File':
         pass
 
 
 class Directory(ABC):
-    def __init__(self, project, name):
+    def __init__(self, project: Project, name: str) -> None:
         self.name = name
         self.project = project
-        self._files = self.get_all_files()
 
     @abstractmethod
-    def delete_directory(self):
+    def delete_directory(self) -> None:
         pass
 
     @abstractmethod
-    def get_file(self, file_name):
+    def get_file(self, file_name: str) -> 'File':
         pass
 
     @abstractmethod
-    def get_all_files(self):
+    def get_all_files(self) -> Sequence['File']:
         pass
 
 
 class File(ABC):
-    def __init__(self, directory, name):
+    def __init__(self, directory: Directory, name: str) -> None:
         self.directory = directory
         self.name = name
 
     @abstractproperty
-    def format(self):
+    def format(self) -> str:
         pass
 
     @abstractproperty
-    def size(self):
+    def size(self) -> int:
         pass
 
     @abstractproperty
-    def data(self):
+    def data(self) -> str:
         pass
 
     @abstractmethod
-    def delete_file(self):
+    def delete_file(self) -> None:
         pass
--- xnat_pacs_data_interface.py
+++ xnat_pacs_data_interface.py
@@ -2,41 +2,47 @@ import datetime
 import os
 from tempfile import TemporaryDirectory
 import zipfile
-from pyxnat import Interface
+from pyxnat import Interface # type: ignore
 import uuid
 from pacs_data_interface import Connection, Project, Directory, File
+from typing import List, Optional, Sequence
 
 
 #---------------------------------------------#
 #          XNAT data interface class          #
 #---------------------------------------------#
 class XNAT(Connection):
-    def __init__(self, username, password):
+    def __init__(self, username: str, password: str):
         # connect to xnat server
         self.interface = Interface(server='http://vm204-misit.informatik.uni-augsburg.de',
                                    user=username,
                                    password=password)
-        self.user = self.interface._user
         super().__init__()
 
-    def __enter__(self):
+    @property
+    def user(self) -> str:
+        return self.interface._user
+
+    def __enter__(self) -> 'XNAT':
         return self
 
-    def __exit__(self, type, value, traceback):
+    def __exit__(self, type, value, traceback) -> None:
         # disconnect from xnat server to quit session
         self.interface.disconnect()
-        print("successful disconnect from server")
+        print("Successful disconnect from server!")
 
     #---------------------------------------#
     #      XNAT Projects data retrieval     #
     #---------------------------------------#
     # get single project
-    def get_project(self, name):
+    def get_project(self, name: str) -> 'XNATProject':
         return XNATProject(self, name)
 
     # get list of project identifiers
-    def get_all_projects(self):
+    def get_all_projects(self) -> Sequence['XNATProject']:
         project_names = self.interface.select.projects().fetchall()
+        if len(project_names) == 0:
+            return []
         projects = []
         for p in project_names:
             project = self.get_project(p)
@@ -48,7 +54,7 @@ class XNAT(Connection):
 #       XNAT Project interface class          #
 #---------------------------------------------#
 class XNATProject(Project):
-    def __init__(self, connection, name):
+    def __init__(self, connection: XNAT, name: str) -> None:
         project = connection.interface.select.project(name)
         if project.exists() != True:
             project.create()
@@ -56,41 +62,44 @@ class XNATProject(Project):
         super().__init__(connection, name)
 
     @property
-    def description(self):
+    def description(self) -> str:
         return self._xnat_project_object.description()
 
     @property
-    def owners(self):
+    def owners(self) -> List[str]:
         return self._xnat_project_object.owners()
 
     @property
-    def your_user_role(self):
+    def your_user_role(self) -> str:
         return self._xnat_project_object.user_role(self.connection.user)
 
     # delete project
-    def delete_project(self):
+    def delete_project(self) -> None:
         project = self._xnat_project_object
         if project.exists():
             project.delete()
 
     # get directory from project
-    def get_directory(self, directory_name):
+    def get_directory(self, directory_name: str) -> 'XNATDirectory':
         return XNATDirectory(self, directory_name)
 
     # get list of project directory objects
-    def get_all_directories(self):
-        project = self._xnat_project_object
+    def get_all_directories(self) -> Sequence['XNATDirectory']:
         directory_names = []
-        for r in project.directories():
+        # get directory names
+        for r in self._xnat_project_object.resources():
             directory_names.append(r.label())
-        directories = []
-        for r in directory_names:
-            directory = self.get_directory(r)
-            directories.append(directory)
-        return directories
+        if len(directory_names) == 0:
+            return []
+        else:
+            directories = []
+            for r in directory_names:
+                directory = self.get_directory(r)
+                directories.append(directory)
+            return directories
 
     # extract zip data and feed it to insert_file_project for file upload to a given project
-    def insert_zip_into_project(self, file_path):
+    def insert_zip_into_project(self, file_path: str) -> 'XNATDirectory':
         directory_name = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
         if zipfile.is_zipfile(file_path):
             with TemporaryDirectory() as tempdir:
@@ -107,7 +116,7 @@ class XNATProject(Project):
         return XNATDirectory(self, directory_name)
 
     # single file upload to given project
-    def insert_file_into_project(self, file_path, directory_name=''):
+    def insert_file_into_project(self, file_path: str, directory_name: str ='') -> 'XNATFile':
         project = self._xnat_project_object
         # file names are unique, duplicate file names can not be inserted
         file_id = str(uuid.uuid4())
@@ -116,41 +125,41 @@ class XNATProject(Project):
             directory_name = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
         if file_path.endswith('.jpg') or file_path.endswith('.jpeg'):
             file_id = file_id + '.jpeg'
-            project.directory(directory_name).file(file_id).insert(
+            project.resource(directory_name).file(file_id).insert(
                 file_path, content='image', format='JPEG', tags='image jpeg')
         if file_path.endswith('.json'):
             file_id = file_id + '.json'
-            project.directory(directory_name).file(file_id).insert(
+            project.resource(directory_name).file(file_id).insert(
                 file_path, content='metadata', format='JSON', tags='metadata')
         if file_path.endswith('.nii'):
             file_id = file_id + '.nii'
-            project.directory(directory_name).file(file_id).insert(
+            project.resource(directory_name).file(file_id).insert(
                 file_path, content='image', format='NIFTI', tags='image nifti')
         # TODO: upload dicom with subject/experiment data structure
         if file_path.endswith('.dcm'):
             file_id = file_id + '.dcm'
-            project.directory(directory_name).file(file_id).insert(
+            project.resource(directory_name).file(file_id).insert(
                 file_path, content='image', format='DICOM', tags='image dicom')
         return XNATFile(XNATDirectory(self, directory_name), file_id)
 
 
 class XNATDirectory(Directory):
-    def __init__(self, project: XNATProject, name):
-        project = project.connection.interface.select.project(project.name)
-        self._xnat_resource_object = project.directory(name)
+    def __init__(self, project: XNATProject, name: str) -> None:
+        p = project.connection.interface.select.project(project.name)
+        self._xnat_resource_object = p.resource(name)
         super().__init__(project, name)
 
     # remove a XNAT resource directory from a project
-    def delete_directory(self):
+    def delete_directory(self) -> None:
         directory = self._xnat_resource_object
         if directory.exists():
             directory.delete()
 
-    def get_file(self, file_name):
+    def get_file(self, file_name: str) -> 'XNATFile':
         return XNATFile(self, file_name)
 
     # get all files from directory
-    def get_all_files(self):
+    def get_all_files(self) -> Sequence['XNATFile']:
         directory = self._xnat_resource_object
         file_names = directory.files().fetchall()
         files = []
@@ -161,26 +170,27 @@ class XNATDirectory(Directory):
 
 
 class XNATFile(File):
-    def __init__(self, directory: XNATDirectory, file_name):
-        self._xnat_file_object = directory._xnat_resource_object.file(file_name)
+    def __init__(self, directory: XNATDirectory, file_name: str) -> None:
+        self._xnat_file_object = directory._xnat_resource_object.file(
+            file_name)
         super().__init__(directory=directory, name=file_name)
 
     @property
-    def format(self):
+    def format(self) -> str:
         return self._xnat_file_object.format()
 
     @property
-    def size(self):
+    def size(self) -> int:
         return int(self._xnat_file_object.size())
 
     @property
-    def data(self):
+    def data(self) -> str:
         # retrieve data to a temporary directory
         with TemporaryDirectory() as tempdir:
             return self._xnat_file_object.get_copy(tempdir + self.name)
 
     # delete file
-    def delete_file(self):
+    def delete_file(self) -> None:
         file = self._xnat_file_object
         if file.exists():
             file.delete()
