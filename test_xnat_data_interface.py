2b278ca2d9d201b8854b58e6476286d93d3b8815
Tamara Krafft <tamara.krafft@uni-a.de>
2022-08-04 11:45:43 +0200

    cleaner tests (less code per test) #23

--- test_xnat_data_interface.py
+++ test_xnat_data_interface.py
@@ -7,10 +7,9 @@ from PIL import Image
 
 
 class TestDataInterface(unittest.TestCase):
-    file_test = '/home/main/Desktop/pacs2go/pacs2go/test_data/dicom_ct_images/CT000000.dcm'
+    file_path = '/home/main/Desktop/pacs2go/pacs2go/test_data/dicom_ct_images/CT000000.dcm'
     zip_file_setup = '/home/main/Desktop/pacs2go/pacs2go/test_data/benchmarking/convert/jpegs_25.zip'
     zip_file_test = '/home/main/Desktop/pacs2go/pacs2go/test_data/benchmarking/convert/jpegs_25.zip'
-    project_name = uuid.uuid4()
     user = 'admin'
     pwd = 'admin'
 
@@ -24,29 +23,42 @@ class TestDataInterface(unittest.TestCase):
     def setUpClass(self):
         # create test data
         with XNAT(self.user, self.pwd) as connection:
-            self.project = XNATProject(connection, self.project_name)
+            self.project = XNATProject(connection, uuid.uuid4())
             self.resource = self.project.insert_zip_into_project(self.zip_file_setup)
             self.file = self.resource.get_all_files()[0]
+            # data to test delete functionalities
+            self.to_be_deleted_project = XNATProject(connection, uuid.uuid4())
+            self.to_be_deleted_resource = self.project.insert_zip_into_project(self.zip_file_setup)
+            self.to_be_deleted_file = self.resource.get_all_files()[1]
+            # name of a project to test create functionality, stored centrally to ensure deletion after test
+            self.to_be_created_project_name = uuid.uuid4()
 
     @classmethod
     def tearDownClass(self):
         # Delete all test data
         with XNAT(self.user, self.pwd) as connection:
             self.project.delete_project()
+            p = connection.get_project(self.to_be_created_project_name)
+            p.delete_project()
+            
 
     # checks if a project with a certain name is really created and deleted
-    def test_create_delete_project(self):
+    def test_create_project(self):
         len_before = len(self.connection.get_all_projects())
-        project = XNATProject(self.connection, uuid.uuid4())
+        project = XNATProject(self.connection, self.to_be_created_project_name)
         self.assertIn(str(project.name), [
                       p.name for p in self.connection.get_all_projects()])
         self.assertEqual(
             len_before + 1, len(self.connection.get_all_projects()))
-        project.delete_project()
-        self.assertNotIn(str(project.name), [
+        
+
+    def test_delete_project(self):
+        len_before = len(self.connection.get_all_projects())
+        self.to_be_deleted_project.delete_project()
+        self.assertNotIn(str(self.to_be_deleted_project.name), [
                       p.name for p in self.connection.get_all_projects()])
         self.assertEqual(
-            len_before, len(self.connection.get_all_projects()))
+            len_before - 1, len(self.connection.get_all_projects()))
 
     # checks if correct number of files was uploaded and if a new directory was created
     def test_insert_zip(self):
@@ -63,31 +75,34 @@ class TestDataInterface(unittest.TestCase):
     # checks if upload of single file with and without specified directory is successful
     def test_insert_file(self):
         len_before = len(self.resource.get_all_files())
-        file = self.project.insert_file_into_project(self.file_test, self.resource.name)
+        file = self.project.insert_file_into_project(self.file_path, self.resource.name)
         self.assertIn(file.name, [f.name for f in self.resource.get_all_files()])
         self.assertEqual(len_before + 1, len(self.resource.get_all_files()))
         # test file upload without specified directory (new directory will be created)
-        file = self.project.insert_file_into_project(self.file_test)
+        file = self.project.insert_file_into_project(self.file_path)
         self.assertIn(file.name, [f.name for f in file.directory.get_all_files()])
 
 
-    # checks if single file and single directory are deleted (z because this should be the last test to be executed)
-    def z_test_delete_file_and_directory(self):
-        #test deletion of file
-        self.assertIn(self.file.name, [f.name for f in self.resource.get_all_files()])
-        self.file.delete_file()
-        self.assertNotIn(self.file.name, [f.name for f in self.resource.get_all_files()])
-        #test deletion of directory (resource)
-        self.assertIn(self.resource.name, [r.name for r in self.project.get_all_directories()])
-        self.resource.delete_directory()
-        self.assertNotIn(self.resource.name, [r.name for r in self.project.get_all_directories()])
-
-    
     # checks if file image data can be retrieved from XNAT
     def test_file_retrieval(self):
         im = Image.open(self.file.data)
         self.assertEqual(im.format, self.file.format)
         self.assertEqual(len(im.fp.read()) + 623 , self.file.size) #not equal for some reason, len(im.fp.read() is always 623byte smaller (metadata perhabs?)
+
+
+    # checks if single file is deleted from directory
+    def test_delete_file(self):
+        self.assertIn(self.to_be_deleted_file.name, [f.name for f in self.resource.get_all_files()])
+        self.to_be_deleted_file.delete_file()
+        self.assertNotIn(self.to_be_deleted_file.name, [f.name for f in self.resource.get_all_files()])
+
+
+    # check if single directory is deleted from project
+    def test_delete_diretory(self):
+        self.assertIn(self.to_be_deleted_resource.name, [r.name for r in self.project.get_all_directories()])
+        self.to_be_deleted_resource.delete_directory()
+        self.assertNotIn(self.to_be_deleted_resource.name, [r.name for r in self.project.get_all_directories()])
+
 
 
 if __name__ == '__main__':
