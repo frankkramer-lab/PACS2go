b2141aea0d2c988d7b73b6835a473a81ec8ddb09
Tamara Krafft <tamara.krafft@uni-a.de>
2022-08-14 22:43:35 +0200

    xnat and dash now run at the same time on different ports, nginx exluded (for now) #24

--- frontend/app.py
+++ frontend/app.py
@@ -49,4 +49,4 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
 ])
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=80,debug=True)
+    app.run(host='0.0.0.0',port=5000,debug=True)
