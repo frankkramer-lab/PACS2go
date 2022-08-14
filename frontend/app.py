39d595f5ffbf0f2c11a815edc0c0b152f0461080
Tamara Krafft <tamara.krafft@uni-a.de>
2022-08-14 23:20:45 +0200

    very simple app.py #24

--- frontend/app.py
+++ frontend/app.py
@@ -1,51 +1,27 @@
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-
 from dash import Dash, dcc, html
-import plotly.express as px
+import dash_bootstrap_components as dbc
 import pandas as pd
 
-app = Dash(name="xnat2go")
+app = Dash(name="xnat2go", external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 colors = {
-    'background': '#111111',
-    'text': '#7FDBFF'
+    'background': '#FFFFFF',
+    'text': '#000000'
 }
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
-
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
-
-fig.update_layout(
-    plot_bgcolor=colors['background'],
-    paper_bgcolor=colors['background'],
-    font_color=colors['text']
-)
-
-app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+app.layout = dbc.Container(style={'backgroundColor': colors['background']}, children=[
     html.H1(
-        children='Hello Dash',
+        children='PACS2go 2.0',
         style={
-            'textAlign': 'center',
+            'textAlign': 'left',
             'color': colors['text']
         }
     ),
-
-    html.Div(children='Dash: A web application framework for your data.', style={
-        'textAlign': 'center',
+    html.Div(children='Exchange medical files.', style={
+        'textAlign': 'left',
         'color': colors['text']
     }),
 
-    dcc.Graph(
-        id='example-graph-2',
-        figure=fig
-    )
 ])
 
 if __name__ == '__main__':
