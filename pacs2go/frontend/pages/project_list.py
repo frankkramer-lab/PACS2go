fd23437f3c6559b9ab341ddd82aa1ec579422135
Tamara Krafft <tamara.krafft@uni-a.de>
2022-09-09 14:33:30 +0200

    removed delete buttons from project list view due to limited support in dash #27

--- pacs2go/frontend/pages/project_list.py
+++ pacs2go/frontend/pages/project_list.py
@@ -23,11 +23,11 @@ def get_projects_table():
     for p in get_projects_list():
         # project names represent links to individual project pages
         rows.append(html.Tr([html.Td(html.A(p.name, href=f"/project/{p.name}", className="text-dark")), html.Td(
-            "You are an " + p.your_user_role + " for this project."), html.Td(len(p.get_all_directories())), html.Td(modal_delete(p))]))
+            "You are an " + p.your_user_role + " for this project."), html.Td(len(p.get_all_directories()))]))
 
     table_header = [
         html.Thead(
-            html.Tr([html.Th("Project Name"), html.Th("Your user role"), html.Th("Number of Directories"), html.Th("Action")]))
+            html.Tr([html.Th("Project Name"), html.Th("Your user role"), html.Th("Number of Directories")]))
     ]
 
     table_body = [html.Tbody(rows)]
@@ -69,37 +69,6 @@ def modal_create():
     ])
 
 
-def modal_delete(project: XNATProject):
-    # caution: id's have to be specific for project otherwise duplicate id's will be overwritten
-    return html.Div([
-        # button which triggers modal activation
-        dbc.Button(html.I(className="bi bi-trash me-2"),
-                   id=f"delete_project_{project.name}", color="danger"),
-        # actual modal view
-        dbc.Modal(
-            [
-                dbc.ModalHeader(dbc.ModalTitle(f"Delete Project")),
-                dbc.ModalBody([
-                    dbc.Label(
-                        "Are you sure that you want to delete this project?"),
-                    dbc.Input(value=project.name, disabled=True,
-                              id=f"to_be_deleted_project_{project.name}")
-                ]),
-                dbc.ModalFooter([
-                    # button which triggers the creation of a project (see modal_and_project_creation)
-                    dbc.Button("Delete Project",
-                               id=f"delete_and_close_{project.name}", color="danger"),
-                    # button which causes modal to close/disappear
-                    dbc.Button(
-                        "Close", id=f"close_modal_delete_{project.name}")
-                ]),
-            ],
-            id=f"modal_delete_{project.name}",
-            is_open=False,
-        ),
-    ])
-
-
 #################
 #   Callbacks   #
 #################
@@ -132,35 +101,6 @@ def modal_and_project_creation(open, close, create_and_close, is_open, project_n
         return is_open, no_update
 
 
-def modal_and_project_deletion(open, close, delete_and_close, is_open, project):
-    # callback for project deletion modal view and executing project deletion
-    # open/close modal via button click
-    if ctx.triggered_id == f"delete_project_{project}" or ctx.triggered_id == f"close_modal_delete_{project}":
-        return not is_open, no_update
-    # user chooses to delete project in modal -> delete project in backend
-    elif ctx.triggered_id == f"delete_and_close_{project}":
-        try:
-            with XNAT(server, "admin", "admin") as connection:
-                p = connection.get_project(project)
-                p.delete_project()
-        except Exception as err:
-            # TODO: differentiate between different exceptions
-            return is_open, dbc.Alert(str(err), color="danger")
-        return not is_open, dbc.Alert(f"Successfully deleted project '{project}'", color="success")
-    else:
-        return is_open, no_update
-
-
-# attach the callback to each modal / button pair for project deletion
-# https://community.plotly.com/t/need-help-with-modal-loading/30247/2
-# TODO: newly created projects can't be deleted! this callback loop is only executed on init
-for p in get_projects_list():
-    callback([Output(f'modal_delete_{p.name}', 'is_open'), Output(f'feedback_{p.name}', 'children')],
-             [Input(f'delete_project_{p.name}', 'n_clicks'), Input(
-              f'close_modal_delete_{p.name}', 'n_clicks'), Input(f'delete_and_close_{p.name}', 'n_clicks')],
-             State(f"modal_delete_{p.name}", "is_open"), State(f"to_be_deleted_project_{p.name}", "value"))(modal_and_project_deletion)
-
-
 #################
 #  Page Layout  #
 #################
@@ -173,14 +113,6 @@ def layout():
             html.Div([html.A(dbc.Button(html.I(className="bi bi-arrow-clockwise"), size='lg'), href='', className="me-2"),
             modal_create()], className="d-flex justify-content-between")
         ], className="d-flex justify-content-between mb-4"),
-        html.Div(delete_feedback()),
         get_projects_table(),
     ])
 
-
-def delete_feedback():
-    # delete project callbacks need unique feedback elements, bc output (feedback) can only be used by one callback
-    f = []
-    for p in get_projects_list():
-        f.append(html.Div(id=f'feedback_{p.name}'))
-    return f
