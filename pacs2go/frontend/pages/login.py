d7db98af64dd0bafd54fa34ef7d41237e43ee80b
Tamara Krafft <tamara.krafft@uni-a.de>
2023-01-07 17:26:39 +0100

    remove dbc.Form #31

--- pacs2go/frontend/pages/login.py
+++ pacs2go/frontend/pages/login.py
@@ -3,15 +3,16 @@ from dash import dcc
 from dash import html
 import dash_bootstrap_components as dbc
 
+
 dash.register_page(__name__)
 
 # Login screen
-layout = dbc.Form(
+layout = html.Form(
     [
-        html.H2("Please log in to continue:", className="mb-3"),
+        html.H2("Please log in to continue:", id="h1"),
         dbc.Input(placeholder="Enter your username", type="text", id="uname-box", name='username', class_name="mb-3"),
         dbc.Input(placeholder="Enter your password", type="password", id="pwd-box", name='password', class_name="mb-3"),
-        dbc.Button("Login", n_clicks=0, type="submit", id="login-button", color='success'),
-        html.Div(id="output-state")
-    ], method='POST', class_name="w-50"
+        dbc.Button(children="Login", n_clicks=0, type="submit", id="login-button", color="success"),
+        html.Div(children="", id="output-state")
+    ], method='POST', className="w-50"
 )
