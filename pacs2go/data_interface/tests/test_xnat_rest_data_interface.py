b9a4aef7816b2ef3b66bc571a31854e688f27148
Tamara Krafft <tamara.krafft@uni-a.de>
2023-01-15 18:05:11 +0100

    create project, Project class #33

--- pacs2go/data_interface/tests/test_xnat_rest_data_interface.py
+++ pacs2go/data_interface/tests/test_xnat_rest_data_interface.py
@@ -1,6 +1,7 @@
 import unittest
+import uuid
 
-from pacs2go.data_interface.xnat_rest_pacs_data_interface import XNAT
+from pacs2go.data_interface.xnat_rest_pacs_data_interface import XNAT, XNATProject
 
 
 class TestConnection(unittest.TestCase):
@@ -36,10 +37,51 @@ class TestDataInterface(unittest.TestCase):
             self.session = connection.session_id
             super(TestDataInterface, self).run(result)
 
+    @classmethod
+    def setUpClass(self):
+        # create test data
+        with XNAT(server=self.server, username=self.user, password=self.pwd, kind="XNAT") as connection:
+            self.project = XNATProject(connection, str(uuid.uuid4()))
+            #self.directory = self.project.insert_zip_into_project(self.zip_file_setup)
+            # data to test delete functionalities
+            #self.to_be_deleted_project = XNATProject(connection, str(uuid.uuid4()))
+            #self.to_be_deleted_directory = self.project.insert_zip_into_project(self.zip_file_setup)
+            #self.to_be_deleted_file = self.project.insert_file_into_project(self.file_path)
+            # name of a project to test create functionality, stored centrally to ensure deletion after test
+            self.to_be_created_project_name = uuid.uuid4()
+
+    @classmethod
+    def tearDownClass(self):
+        # Delete all test data
+        with XNAT(server=self.server, username=self.user, password=self.pwd, kind="XNAT") as connection:
+            if self.project.exists():
+                self.project.delete_project()
+            try:
+                p = connection.get_project(str(self.to_be_created_project_name))
+                p.delete_project()
+            except:
+                pass
+
     def test_getproject(self):
         # Checks if a project with a certain name is really created
         p = self.connection.get_project("test_keywords_2")
-        print(p.name)
+        #print(p.name)
+
+    def test_getallprojects(self):
+        p = self.connection.get_all_projects()
+        #print(p[0].name)
+
+    def test_createproject(self):
+        # Checks if a project with a certain name is really created
+        len_before = len(self.connection.get_all_projects())
+        project = XNATProject(self.connection, str(
+            self.to_be_created_project_name))
+        #print(project.description)
+        self.assertIn(str(project.name), [
+                      p.name for p in self.connection.get_all_projects()])
+        self.assertEqual(
+            len_before + 1, len(self.connection.get_all_projects()))
+
 
 
 if __name__ == '__main__':
--- pacs2go/data_interface/xnat_rest_pacs_data_interface.py
+++ pacs2go/data_interface/xnat_rest_pacs_data_interface.py
@@ -7,7 +7,7 @@ import requests
 
 
 class XNAT():
-    def __init__(self, server: str, username: str, password: str= None, session_id:str=None, kind: str = None) -> None:
+    def __init__(self, server: str, username: str, password: str = None, session_id: str = None, kind: str = None) -> None:
         self.server = server
         self.username = username
         # User may either specify password of session_id to authenticate themselves
@@ -17,26 +17,28 @@ class XNAT():
             response = requests.post(
                 server + "/data/services/auth", data=data, headers=headers)
             if response.status_code != 200:
-                raise Exception("Something went wrong connecting to XNAT. " + str(response.text))
+                raise Exception(
+                    "Something went wrong connecting to XNAT. " + str(response.text))
             else:
                 # Return SessionID
                 self.session_id = response.text
                 self.cookies = {"JSESSIONID": self.session_id}
-                print(requests.get(self.server + "/xapi/users/username", cookies=self.cookies).text)
+                # print(requests.get(self.server + "/xapi/users/username",cookies=self.cookies).text)
         elif session_id != None:
             self.session_id = session_id
             self.cookies = {"JSESSIONID": self.session_id}
         else:
-            raise Exception("You must eiter specify a password or a session_id to authenticate.")
+            raise Exception(
+                "You must eiter specify a password or a session_id to authenticate.")
 
-        
     @property
     def _kind(self) -> str:
         return "XNAT"
 
     @property
     def user(self) -> str:
-        response = requests.get(self.server + "/xapi/users/username", cookies=self.cookies)
+        response = requests.get(
+            self.server + "/xapi/users/username", cookies=self.cookies)
         if response.status_code == 200:
             # User was found, return username
             return response.text
@@ -50,26 +52,48 @@ class XNAT():
     def __exit__(self, type, value, traceback) -> None:
         # TODO: invalidate Session_id
         pass
-        
-    def get_project(self, name: str) -> Optional['XNATProject']:
-        response = requests.get(self.server + f"/data/projects/{name}", cookies=self.cookies)
-        print(response)
+
+    def create_project(self, name: str) -> Optional['XNATProject']:
+        # A Project with the name 'name' does not exist yet, create one
+        headers = {'Content-Type': 'application/xml'}
+        project_data = f"""
+            <projectData>
+            <ID>{name}</ID>
+            <name>{name}</name>
+            <description>This is a new project.</description>
+            </projectData>
+            """
+        response = requests.post(self.server + "/data/projects", headers=headers, data=project_data, cookies=self.cookies)
         if response.status_code == 200:
+            print("Project created successfully")
             return XNATProject(self, name)
         else:
+            raise Exception(
+                "Something went wrong trying to create a new project." + str(response.status_code))
+
+    def get_project(self, name: str) -> Optional['XNATProject']:
+        response = requests.get(
+            self.server + f"/data/projects/{name}", cookies=self.cookies)
+        if response.status_code == 200:
+            return XNATProject(self, name, just_get=True)
+        else:
             # User not found, return False
-            raise Exception("Project not found." + str(response.status_code))
+            raise Exception(
+                f"Project '{name}' not found." + str(response.status_code))
 
     def get_all_projects(self) -> List['XNATProject']:
-        response = requests.get(self.server + "/xapi/users/projects", cookies=self.cookies)
+        response = requests.get(
+            self.server + "/xapi/users/projects", cookies=self.cookies)
         if response.status_code == 200:
             # User was found, return username
-            project_names = response.text
+            project_names = response.json()
             if len(project_names) == 0:
+                # No projects yet
                 return []
 
             projects = []
             for p in project_names:
+                # Create List of all Project objectss
                 project = self.get_project(p)
                 projects.append(project)
 
@@ -86,41 +110,65 @@ class XNAT():
 
 
 class XNATProject():
-    def __init__(self, connection: XNAT, name: str) -> None:
+    def __init__(self, connection: XNAT, name: str, just_get: bool = False) -> None:
         self.connection = connection
+        self.cookies = self.connection.cookies
         self.name = name
 
+        if just_get is False:
+            # just_get is used to internally optimize the number of XNAT API calls
+            response = requests.get(
+                self.connection.server + f"/data/projects/{self.name}", cookies=self.cookies)
+            if response.status_code != 200:
+                # Project in XNAT does not exist yet, so it is inserted into XNAT
+                self.connection.create_project(self.name)
 
-#     @property
-#     def description(self) -> str:
-#         pass
+    @property
+    def description(self) -> str:
+        response = requests.get(
+            self.connection.server + f"/data/projects/{self.name}", cookies=self.cookies)
+        if response.status_code == 200:
+            return response.json()['description']
+        else:
+            raise Exception(
+                'Something went wrong trying to retrieve the description' + str(response.status_code))
 
-#     def set_description(self, description_string: str) -> None:
-#         pass
+    def set_description(self, description_string: str) -> None:
+        pass
 
-#     @property
-#     def keywords(self) -> str:
-#         pass
+    @property
+    def keywords(self) -> str:
+        pass
 
-#     def set_keywords(self, keywords_string: str) -> None:
-#         pass
+    def set_keywords(self, keywords_string: str) -> None:
+        pass
 
-#     @property
-#     def owners(self) -> List[str]:
-#         pass
+    @property
+    def owners(self) -> List[str]:
+        pass
 
-#     @property
-#     def your_user_role(self) -> str:
-#         pass
+    @property
+    def your_user_role(self) -> str:
+        pass
 
-#     def exists(self) -> bool:
-#         pass
+    def exists(self) -> bool:
+        response = requests.get(
+            self.connection.server + f"/data/projects/{self.name}", cookies=self.cookies)
+        if response.status_code == 200:
+            return True
+        else:
+            return False
 
-#     def download(self, destination: str) -> str:
-#         pass
+    def download(self, destination: str) -> str:
+        pass
 
-#     def delete_project(self) -> None:
-#         pass
+    def delete_project(self) -> None:
+        response = requests.delete(
+            self.connection.server + f"/data/projects/{self.name}", cookies=self.cookies)
+        if response.status_code != 200:
+            raise Exception(
+                'Something went wrong trying to delete the project.' + str(response.status_code))
+
 
 #     def get_directory(self, name) -> 'Directory':
 #         pass
