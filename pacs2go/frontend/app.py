6b84017feef4604d900ce2412a684c5c40e24774
Tamara Krafft <tamara.krafft@uni-a.de>
2023-03-07 19:50:54 +0100

    minor bug fixes + gunicorn deployment

--- pacs2go/frontend/app.py
+++ pacs2go/frontend/app.py
@@ -213,4 +213,4 @@ def login_feedback(path):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True, dev_tools_hot_reload=False)
+    app.run(host='0.0.0.0', port=5000, debug=True)
