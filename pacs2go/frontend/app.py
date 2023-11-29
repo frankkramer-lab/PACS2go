f94c877da9f1eaca07d70afbe31522e90418e8b2
Tamara Krafft <tamara.krafft@uni-a.de>
2023-11-29 16:22:44 +0100

    prod setting

--- pacs2go/frontend/app.py
+++ pacs2go/frontend/app.py
@@ -183,4 +183,4 @@ if __name__ == '__main__':
     #         restrictions=[50],
     #     )
     # Start App
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
