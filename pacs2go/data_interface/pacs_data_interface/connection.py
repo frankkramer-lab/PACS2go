31ba49767d607a63a1767132927863660d1554ce
krafftta <tamara.krafft@uni-a.de>
2024-05-23 10:49:37 +0200

    docstrings for connection.py

--- pacs2go/data_interface/pacs_data_interface/connection.py
+++ pacs2go/data_interface/pacs_data_interface/connection.py
@@ -15,9 +15,24 @@ from pacs2go.data_interface.xnat_rest_wrapper import XNAT
 
 
 class Connection:
+    """Handles the connection to PACS system and provides methods for interacting with projects, directories, and files."""
+    
     this_timezone = timezone("Europe/Berlin")
 
-    def __init__(self, server: str, username: str, password: str = '', session_id: str = '', kind: str = '', db_host: str = 'data-structure-db', db_port: int = 5432) -> None:
+    def __init__(self, server: str, username: str, password: str = '', session_id: str = '', kind: str = '') -> None:
+        """
+        Initializes the connection to the PACS server (accessible via _file_store_connection property).
+
+        Args:
+            server (str): The server address.
+            username (str): The username for authentication.
+            password (str, optional): The password for authentication. Defaults to ''.
+            session_id (str, optional): The session ID for authentication. Defaults to ''.
+            kind (str, optional): The type of connection (e.g., 'XNAT'). Defaults to ''.
+
+        Raises:
+            FailedConnectionException: If the connection type is unsupported or any exception occurs during connection initiation.
+        """
         self.kind = kind
         self.server = server
         self.username = username
@@ -43,6 +58,15 @@ class Connection:
 
     @property
     def _kind(self) -> str:
+        """
+        Returns the kind of the connection.
+
+        Returns:
+            str: The kind of the connection. E.g.: 'XNAT'
+
+        Raises:
+            FailedConnectionException: If the connection type is unsupported.
+        """
         if self.kind in ['XNAT']:
             return self.kind
         else:
@@ -53,6 +77,15 @@ class Connection:
 
     @property
     def user(self) -> str:
+        """
+        Retrieves the user information.
+
+        Returns:
+            str: The username.
+
+        Raises:
+            FailedConnectionException: If the user information cannot be retrieved.
+        """
         try:
             return self._file_store_connection.user
         except Exception as e:
@@ -63,6 +96,15 @@ class Connection:
     
     @property
     def all_users(self) -> list:
+        """
+        Retrieves all users from the connection.
+
+        Returns:
+            list: List of all users.
+
+        Raises:
+            FailedConnectionException: If the user information cannot be retrieved.
+        """
         try:
             return self._file_store_connection.all_users
         except Exception as e:
@@ -72,6 +114,15 @@ class Connection:
             raise FailedConnectionException
 
     def __enter__(self) -> 'Connection':
+        """
+        Enters the context manager. Necessary along with __exit__ to support Context Management usage.
+
+        Returns:
+            Connection: The connection instance.
+
+        Raises:
+            FailedConnectionException: If unable to enter the context.
+        """
         try:
             self._file_store_connection = self._file_store_connection.__enter__()
             return self
@@ -82,6 +133,17 @@ class Connection:
             raise FailedConnectionException
 
     def __exit__(self, type, value, traceback) -> None:
+        """
+        Exits the context manager.
+
+        Args:
+            type: The exception type.
+            value: The exception value.
+            traceback: The traceback object.
+
+        Raises:
+            FailedDisconnectException: If unable to exit the context.
+        """
         try:
             self._file_store_connection.__exit__(type, value, traceback)
         except Exception as e:
@@ -91,6 +153,22 @@ class Connection:
             raise FailedDisconnectException
 
     def create_project(self, name: str, description: str = '', keywords: str = '', parameters: str = '') -> 'Project':
+        """
+        Creates a new project in the file store and database.
+
+        Args:
+            name (str): The name of the project.
+            description (str, optional): The description of the project. Defaults to ''.
+            keywords (str, optional): The keywords associated with the project. Defaults to ''.
+            parameters (str, optional): Additional parameters for the project. Defaults to ''.
+
+        Returns:
+            Project: The created project instance.
+
+        Raises:
+            UnsuccessfulCreationException: If project creation fails.
+        """
+        
         # Remove unallowed chars
         name = name.replace(".","")
         name = name.replace(",","")
@@ -117,6 +195,18 @@ class Connection:
             raise UnsuccessfulCreationException(f"{str(name)}")
 
     def get_project(self, name: str) -> Optional['Project']:
+        """
+        Retrieves a project by name.
+
+        Args:
+            name (str): The name of the project.
+
+        Returns:
+            Optional[Project]: The retrieved project instance.
+
+        Raises:
+            UnsuccessfulGetException: If the project cannot be retrieved.
+        """
         try:
             project = Project(self, name)
             logger.debug(f"User {self.user} retrieved information about project {project.name}.")
@@ -125,20 +215,19 @@ class Connection:
             msg = f"Failed to get Project '{name}'."
             logger.exception(msg)
             raise UnsuccessfulGetException(f"Project '{name}'")
-        
-    #p.your_user_role != '' or current_user.id=='admin'
 
-    def get_all_projects(self, only_accessible:bool = False) -> List['Project']:    
-        """Retrieves a list of all projects from DB
-        
+    def get_all_projects(self, only_accessible: bool = False) -> List['Project']:    
+        """
+        Retrieves a list of all projects from the database.
+
         Args:
-            only_accessible (boolean): if set to True then only projects which the user has rights to are retrieved
-
-        Raises:
-            UnsuccessfulGetException
+            only_accessible (bool, optional): If set to True, only projects which the user has rights to are retrieved. Defaults to False.
 
         Returns:
-            List['Project']: List of Project objects
+            List[Project]: List of Project objects.
+
+        Raises:
+            UnsuccessfulGetException: If unable to retrieve projects.
         """
 
         try:
@@ -155,6 +244,19 @@ class Connection:
             raise UnsuccessfulGetException(f"Projects")
 
     def get_directory(self, project_name: str, directory_name: str) -> Optional['Directory']:
+        """
+        Retrieves a directory by name from a specified project.
+
+        Args:
+            project_name (str): The name of the project.
+            directory_name (str): The name of the directory.
+
+        Returns:
+            Optional[Directory]: The retrieved directory instance.
+
+        Raises:
+            UnsuccessfulGetException: If the directory cannot be retrieved.
+        """
         try:
             d = Directory(self.get_project(project_name), directory_name)
             logger.debug(f"User {self.user} retrieved information about directory {d.unique_name}.")
@@ -166,6 +268,20 @@ class Connection:
                 f"Directory '{directory_name}'")
 
     def get_file(self, project_name: str, directory_name: str, file_name: str) -> Optional['File']:
+        """
+        Retrieves a file by name from a specified directory in a project.
+
+        Args:
+            project_name (str): The name of the project.
+            directory_name (str): The name of the directory.
+            file_name (str): The name of the file.
+
+        Returns:
+            Optional[File]: The retrieved file instance.
+
+        Raises:
+            UnsuccessfulGetException: If the file cannot be retrieved.
+        """
         try:
             f = File(directory=self.get_directory(
                 project_name=project_name, directory_name=directory_name), name=file_name)
@@ -176,6 +292,18 @@ class Connection:
             raise UnsuccessfulGetException(f"File '{file_name}'")
 
     def get_favorites(self, username:str) -> List['Directory']:
+        """
+        Retrieves the list of favorited directories for a user.
+
+        Args:
+            username (str): The username whose favorites are to be retrieved.
+
+        Returns:
+            List[Directory]: List of favorited directories.
+
+        Raises:
+            UnsuccessfulGetException: If the favorites cannot be retrieved.
+        """
         try:
             with PACS_DB() as db:
                 favs = db.get_favorites_by_user(username)
